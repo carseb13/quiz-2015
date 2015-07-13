ade087fe341a619ab5a32dca1bd130f90d7860c0
carseb13 <carseb13@gmail.com>
2015-07-13 01:38:43 -0400

    Nuevo juego para el quiz

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+exports.question= function(req,res){
+	res.render('/quizes/question',{pregunta: 'Capital de Italia'});
+}
+
+exports.answer= function(req,res){
+	if(req.query.respuesta.toLowerCase()==='roma'){
+		res.render('/quizes/answer',{respuesta:'Correcto'});				
+	}else{
+		res.render('/quizes/answer',{respuesta:'Incorrecto'});				
+	}	
+
+
+}
+
