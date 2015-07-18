d81354d7813c3eaa81aec5b7a64182d017edc298
carseb13 <carseb13@gmail.com>
2015-07-18 19:44:07 -0400

    agregando estilos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,15 @@
+//se exporta y disponibiliza los metodos question y answer
 exports.question= function(req,res){
-	res.render('/quizes/question',{pregunta: 'Capital de Italia'});
+	res.render('quizes/question',{pregunta: 'Capital de Italia'});
+	//res.send('<h1>Y DESPUESSS!?!?!?</h1>');
 }
 
 exports.answer= function(req,res){
+	
 	if(req.query.respuesta.toLowerCase()==='roma'){
-		res.render('/quizes/answer',{respuesta:'Correcto'});				
+		res.render('quizes/answer',{respuesta:'Correcto'});				
 	}else{
-		res.render('/quizes/answer',{respuesta:'Incorrecto'});				
+		res.render('quizes/answer',{respuesta:'Incorrecto'});				
 	}	
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+//se importa el controller para las acciones del quiz.
 var quizController  = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
+	//se enruta la salida para la raiz del sitio
   res.render('index', { title: 'QUIZ 2015' });
 });
 
+//se configura que la ruta quizes/question sera manejada con el controlador quizController
 router.get('/quizes/question',quizController.question);
-
 router.get('/quizes/answer',quizController.answer);
 module.exports = router;
