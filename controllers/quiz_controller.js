af9da15c11273df02d233592040db80df50cdc1f
Carlin Esquivel <carseb13@gmail.com>
2015-08-03 00:51:40 -0400

    corregido error al borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -109,11 +109,9 @@ exports.update =function(req,res){
 }
 
 exports.destroy = function(req,res){
-	req.quiz.destroy().then(function(req,res){
-		req.quiz.destroy().then(function(){
-			res.redirect('/quizes');
-		}).catch(function(error){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){
 			next(error);
-		});
 	});
 }
