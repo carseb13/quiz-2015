b440e29d524922fbef22b81582aab367059abfdd
Carlin Esquivel <carseb13@gmail.com>
2015-08-03 00:40:23 -0400

    eliminar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -107,3 +107,13 @@ exports.update =function(req,res){
 		}
 	});	
 }
+
+exports.destroy = function(req,res){
+	req.quiz.destroy().then(function(req,res){
+		req.quiz.destroy().then(function(){
+			res.redirect('/quizes');
+		}).catch(function(error){
+			next(error);
+		});
+	});
+}
